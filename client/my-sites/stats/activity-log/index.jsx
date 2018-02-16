e7a16693cb356691071a76a1c12a7620398551e5
Dennis Snell <dennis.snell@automattic.com>
2018-02-16 18:36:44 -0500

    Rearrange and allow for multi-resource response

--- client/my-sites/stats/activity-log/index.jsx
+++ client/my-sites/stats/activity-log/index.jsx
@@ -61,6 +61,7 @@ import {
 	getSiteTimezoneValue,
 	getOldestItemTs,
 } from 'state/selectors';
+import * as ids from 'state/resource-ids';
 
 const flushEmptyDays = days => [
 	days.length === 1 ? 'empty-day' : 'empty-range',
@@ -646,7 +647,7 @@ export default connect(
 			siteId,
 			siteTitle: getSiteTitle( state, siteId ),
 			slug: getSiteSlug( state, siteId ),
-			tags: getHttpData( 'tags' ).data,
+			tags: getHttpData( ids.readerTags() ).data,
 			timezone,
 			oldestItemTs: getOldestItemTs( state, siteId ),
 		};
