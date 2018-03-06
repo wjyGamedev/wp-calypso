71b45412f3bbec7aa3f6ed7d549592fcfa0ca3a0
Mark Ryall <mark@ryall.name>
2018-03-06 12:42:51 +1000

    add quantity to GA ecommerce:addItem call

--- client/lib/analytics/ad-tracking.js
+++ client/lib/analytics/ad-tracking.js
@@ -662,6 +662,7 @@ function recordProduct( product, orderId ) {
 			name: product.product_slug,
 			price: product.cost,
 			currency: product.currency,
+			quantity: 1,
 		} );
 
 		// Google AdWords
