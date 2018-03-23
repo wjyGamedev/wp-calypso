2548d49bdf0112fa640fefc14f7ba91fe45a124e
Stéphane Thomas <email@stephanethomas.com>
2018-03-23 16:14:05 +0100

    Google My Business: Add New Account page

--- client/my-sites/google-my-business/index.js
+++ client/my-sites/google-my-business/index.js
@@ -8,19 +8,35 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import { navigation, siteSelection, sites } from 'my-sites/controller';
-import { selectBusinessType } from './controller';
 import config from 'config';
+import { navigation, siteSelection, sites } from 'my-sites/controller';
+import { newAccount, selectBusinessType } from './controller';
 import { makeLayout, render as clientRender } from 'controller';
 
 export default function() {
 	if ( config.isEnabled( 'google-my-business' ) ) {
-		page( '/google-my-business', siteSelection, sites, makeLayout, clientRender );
 		page(
-			'/google-my-business/:site_id/',
+			'/google-my-business',
+			siteSelection,
+			sites,
+			makeLayout,
+			clientRender
+		);
+
+		page(
+			'/google-my-business/:site_id',
 			siteSelection,
 			navigation,
 			selectBusinessType,
+			makeLayout,
+			clientRender
+		);
+
+		page(
+			'/google-my-business/new/:site_id',
+			siteSelection,
+			navigation,
+			newAccount,
 			makeLayout,
 			clientRender
 		);
